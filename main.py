f86d0be400104a1218d594bb6ab94caad42835e1
A1Fizzy <artemarhunin@gmail.com>
2022-10-22 14:16:25 +0400

    class Order

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import math
 
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
 
     def __str__(self):
         return f'({self.x}, {self.y})'
@@ -26,10 +24,10 @@ p3 = Point(3, 5)
 p4 = Point(0, 4)
 p5 = Point(2, 5)
 pc1 = Point(4, 0)
-pc2 = Point(9,3)
-pc3 = Point(3,9)
-pc4 = Point(2,2)
-pc5 = Point(8,8)
+pc2 = Point(9, 3)
+pc3 = Point(3, 9)
+pc4 = Point(2, 2)
+pc5 = Point(8, 8)
 
 class Order:
     def __init__(self, number, cords):
@@ -44,20 +42,21 @@ o2 = Order(2, p2)
 o3 = Order(3, p3)
 o4 = Order(4, p4)
 o5 = Order(5, p5)
-print(o1, o2, o3, o4, o5, sep = '\n')
+print(o1, o2, o3, o4, o5, sep='\n')
 
 class Courier:
-    def __init__(self, name, cordsC):
+    def __init__(self, name, cordsC, speed):
         self.name = name
         self.cordsC = cordsC
+        self.speed = speed
 
     def __str__(self):
-        return f'Курьер {self.name}с координатами {self.cordsC}'
+        return f'Курьер {self.name}с координатами {self.cordsC} имеет скорость {self.speed}'
 
-c1 = Courier('Walt ', pc1)
-c2 = Courier('Flin ', pc2)
-c3 = Courier('Jessi ', pc3)
-c4 = Courier('Hank ', pc4)
-c5 = Courier('Brandon ', pc5)
+c1 = Courier('Walt ', pc1, 10)
+c2 = Courier('Flin ', pc2, 10)
+c3 = Courier('Jessi ', pc3, 10)
+c4 = Courier('Hank ', pc4, 10)
+c5 = Courier('Brandon ', pc5, 10)
 
-print ('\n',c1,c2,c3,c4,c5, sep = '\n')
+print('\n', c1, c2, c3, c4, c5, sep='\n')
