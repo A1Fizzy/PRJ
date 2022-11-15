d9833521f7504d6297ddb4941ea424a903f75a87
A1Fizzy <artemarhunin@gmail.com>
2022-11-15 21:25:28 +0400

    work

---
+++ work.py
@@ -0,0 +1,9 @@
+name = input("Введите имя ")
+surname = input("Введите фамилию ")
+number = input("Введите номер группы ")
+
+name = str(name)
+surname = str(surname)
+number = str(number)
+
+print(f"{name} {surname} ({number})")
