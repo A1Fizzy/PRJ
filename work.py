a9c354765d540a4965b15153ee287365af0b8814
A1Fizzy <114985264+A1Fizzy@users.noreply.github.com>
2022-11-19 16:13:04 +0400

    Delete work.py

--- work.py
+++
@@ -1,9 +0,0 @@
-name = input("Введите имя ")
-surname = input("Введите фамилию ")
-number = input("Введите номер группы ")
-
-name = str(name)
-surname = str(surname)
-number = str(number)
-
-print(f"{name} {surname} ({number})")
